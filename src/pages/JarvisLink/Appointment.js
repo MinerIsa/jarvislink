9d63e510d252191eff092e18bcdbaa906356623a
MinerIsa <abc@MacBook-Pro.local>
2023-01-03 13:30:55 -0600

    wall switches

--- src/pages/JarvisLink/Appointment.js
+++ src/pages/JarvisLink/Appointment.js
@@ -106,13 +106,13 @@ const Appointment = () => {
                     Control zones with hard-wired or wire-free kinetic switches.
                   </p>
 
-                  <Link
-                    to="#"
+                  <a
+                    href="https://drive.google.com/file/d/1SHFrKHlQOVJ9ZJarBm74Ai6oKvw8Hmhw/view?usp=sharing"
                     className="btn btn-pills btn-soft-success mt-4"
-                    onClick={() => setModal(true)}
+                    // onClick={() => setModal(true)}
                   >
                     Download PDF
-                  </Link>
+                  </a>
                 </CardBody>
               </Card>
             </Col>
