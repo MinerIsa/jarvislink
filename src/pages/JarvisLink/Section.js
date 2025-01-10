35d8d5208e12e9fb28b7e15be6b6d0cb2f0c33f1
MinerIsa <34116224+MinerIsa@users.noreply.github.com>
2025-01-10 14:19:30 -0600

    Update Section.js

--- src/pages/JarvisLink/Section.js
+++ src/pages/JarvisLink/Section.js
@@ -119,7 +119,7 @@ const Section = () => {
                   data-aos="fade-up"
                   data-aos-duration="1000"
                 >
-                  <span className="text-primary">Advanced</span> controls.
+                  <span className="text-primary">Advanced</span> control.
                   <br />
                   <span className="text-primary">
                     Simple
