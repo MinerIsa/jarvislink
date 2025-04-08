cb1f61fb9653bd873047b27bed3a0e2e9ba37bdf
MinerIsa <34116224+MinerIsa@users.noreply.github.com>
2025-04-08 08:49:11 -0500

    Update GoogleMap.js

--- src/pages/JarvisLink/GoogleMap.js
+++ src/pages/JarvisLink/GoogleMap.js
@@ -9,7 +9,7 @@ class GoogleMap extends Component {
           <Row>
             <Col xs="12" className="p-0">
               <div className="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.6490660350523!2d-92.95926182371186!3d44.950801571070265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f7d7e839891125%3A0xc2f4d3b116b47d66!2s7300%20Hudson%20Blvd%20N%2C%20Oakdale%2C%20MN%2055128!5e0!3m2!1sen!2sus!4v1744119597243!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.6490660350523!2d-92.95926182371186!3d44.950801571070265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f7d7e839891125%3A0xc2f4d3b116b47d66!2s7300%20Hudson%20Blvd%20N%2C%20Oakdale%2C%20MN%2055128!5e0!3m2!1sen!2sus!4v1744119597243!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </Col>
           </Row>
