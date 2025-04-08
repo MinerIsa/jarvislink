1c894bfb2f95497be4c8745954bda222fbe45a0b
MinerIsa <34116224+MinerIsa@users.noreply.github.com>
2025-04-08 09:22:29 -0500

    Update GoogleMap.js

--- src/pages/JarvisLink/GoogleMap.js
+++ src/pages/JarvisLink/GoogleMap.js
@@ -11,9 +11,13 @@ class GoogleMap extends Component {
               <div className="map">
                 <iframe
                   title="uniqueTitle"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2962.4467550351583!2d-88.05518488416449!3d42.055042479208566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880faf78f715cf8d%3A0xcfc4f5a7c60b6fa4!2s1054%20National%20Pkwy%2C%20Schaumburg%2C%20IL%2060173!5e0!3m2!1sen!2sus!4v1669064369970!5m2!1sen!2sus"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.30116302775!2d-88.0188727237281!3d41.90788417123845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fad56aed9e389%3A0xb506dba15743c787!2s951%20N%20Main%20St%20Unit%20A%2C%20Lombard%2C%20IL%2060148!5e0!3m2!1sen!2sus!4v1744121493050!5m2!1sen!2sus"
+                  width="600"
+                  height="450"
                   style={{ border: '0' }}
                   allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
               </div>
             </Col>
