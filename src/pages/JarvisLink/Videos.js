8497de9b36d392db50850f65b98ae9a949c78c81
MinerIsa <34116224+MinerIsa@users.noreply.github.com>
2025-07-31 15:45:54 -0500

    Update Videos.js

--- src/pages/JarvisLink/Videos.js
+++ src/pages/JarvisLink/Videos.js
@@ -9,7 +9,7 @@ const videoCards = [
       'https://cdn.shopify.com/s/files/1/0406/9018/8439/files/Roboto_6.png?v=1723842913',
     title: 'Software',
     subtitle: 'Changing Settings in Jarvis Link',
-    videoUrl: 'https://youtu.be/ujB_YQO3XWA',
+    videoUrl: 'https://www.youtube.com/watch?v=ujB_YQO3XWA',
     description: (
       <>
         <h3>Changing Settings in Jarvis Link.</h3>
